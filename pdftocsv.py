b62e31b27ea983ecd7df04a799d905f9dbd11949
Manoj Nain <manojnain24@gmail.com>
2020-08-27 20:49:18 +0530

    intial code to extract data from pdf

---
+++ pdftocsv.py
@@ -0,0 +1,34 @@
+#Ceating function to extract data from PDF using Pdfminer 
+
+import io
+
+import pdfminer
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+
+# Perform layout analysis for all text
+laparams = pdfminer.layout.LAParams()
+setattr(laparams, 'all_texts', True)
+
+def extract_text_from_pdf(pdf_path):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=laparams)
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+    with open(pdf_path, 'rb') as fh:
+        for page in PDFPage.get_pages(fh, 
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+
+    if text:
+        return text 
