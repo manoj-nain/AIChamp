d779c632ac61ae204c5f58cf2c955a0fd1aec14d
Manoj Nain <manojnain24@gmail.com>
2020-08-28 08:46:05 +0530

    Cleaning data for nlp

---
+++ nlp.py
@@ -0,0 +1,21 @@
+#Using nlp to remove stopwords and finding most frequesnt and essential words
+
+#Creating Pandas Data Frame
+
+import pandas as pd
+profile_df = pd.read_csv('Profile1.csv',names=['Index','Profile'])
+profile_df.head()
+
+#creatinf function to clean text data to remove symbols
+import re
+def clean_data(text):
+    text = re.sub('RT@[\w]*:',"",text)
+    text = re.sub('@[\w]*',"",text)
+    text = re.sub('https?://[A-Za-z0-9./]*',"",text)
+    text = re.sub('\n',"",text)
+    text = re.sub("\."," ",text) 
+    text = re.sub("/"," ",text)
+    return text
+
+#using clean_data function
+profile_df['Profile'] = profile_df['Profile'].apply(lambda x: clean_data(x))
